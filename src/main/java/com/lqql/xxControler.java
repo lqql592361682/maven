9625174a10071aaee2872bbe7135718cbb0aff5d
lqql592361682 <44882318+lqql592361682@users.noreply.github.com>
2020-03-18 17:28:54 +0800

    Update xxControler.java

--- src/main/java/com/lqql/xxControler.java
+++ src/main/java/com/lqql/xxControler.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class xxControler {
     @RequestMapping("/xx")
     public String index() {
-        return "ssssssssxx";
+        return "xx";
     }
 }
